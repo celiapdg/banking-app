548fc360beb16b5726f00957f6994f5e70c71823
Celia <celiapdg@hotmail.com>
2021-02-18 14:12:38 +0100

    Changed project name

--- src/main/java/com/ironhack/bankapp/repository/users/RoleRepository.java
+++ src/main/java/com/ironhack/bankapp/repository/users/RoleRepository.java
@@ -2,6 +2,8 @@ package com.ironhack.bankapp.repository.users;
 
 import com.ironhack.bankapp.model.users.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
